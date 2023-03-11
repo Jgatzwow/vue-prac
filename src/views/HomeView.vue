b06da919c88888057d004e1fca02ca3ccf39d932
Mykhailo <mihasokol1993@gmail.com>
2023-03-11 19:10:48 +0200

    studied some basic example of using refs reactive objects and nonreactive data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="home">
+    <h2>{{appTitle}}</h2>
+    <h3>{{ counterData.title }}</h3>
     <div class="wrapper">
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+    <div class="edit">
+      <h3> Edit counters Title</h3>
+      <input type="text" v-model="counterData.title ">
     </div>
   </div>
 </template>
 
 <script setup>
 
-import {ref} from "vue";
+import {reactive} from "vue";
 
-const counter = ref(0)
+/*const counter = ref(0)
+const counterTitle = ref('tle')*/
+
+const appTitle = 'My counter App'
+
+const counterData = reactive({
+  count: 10,
+  title: 'Counter title'
+})
 
 const increaseCounter = () => {
-  counter.value++
+  counterData.count++
 }
 
 const decreaseCounter = () => {
-  counter.value--
+  counterData.count--
 }
 
 </script>
